877bd26642df6b8825bd321e4e1cf2fb20156d62
saramccombs <theSaraMcCombs@gmail.com>
2020-02-14 15:04:57 -0500

    Removed @user instance and utilized current_user helper instead

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def hello
-    @user = current_user if user_signed_in?
   end
 
   protected
