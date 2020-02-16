27af10dbcbbcd526ce17d72678bcd46b0c2ce4fc
saramccombs <theSaraMcCombs@gmail.com>
2020-02-16 18:55:16 -0500

    Added two helper methods to find parent or child from current user and other family parent accounts from current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :account_from_current_user, :family_parents_from_current_user
 
   def hello
   end
@@ -8,7 +9,23 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     # devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+  end
+
+  # TODO Review with Jake where helper methods are best implemented
+
+  def account_from_current_user
+    if current_user.account_type == "Parent"
+      Parent.find_by(id: current_user.account_id)
+    elsif current_user.account_type == "Child"
+      Child.find_by(id: current_user.account_id)
+    else
+      nil
+    end
+  end
+
+  def family_parents_from_current_user
+    Parent.where(team_name: account_from_current_user.team_name)
   end
 end
