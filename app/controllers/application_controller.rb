e82e50bf9568ab05bd4ba77de59bc7de84e881c5
saramccombs <theSaraMcCombs@gmail.com>
2020-02-17 15:45:22 -0500

    Updated application controller to permit nested parent attributes in the params.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, parent_attributes: [:name, :team_name]])
     # devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
   end
 
