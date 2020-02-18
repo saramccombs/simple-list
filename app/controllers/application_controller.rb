74a7436ba0686b225c634e349610b5bfa6b5cb3d
saramccombs <theSaraMcCombs@gmail.com>
2020-02-18 15:27:35 -0500

    Refactored account_from_current_user to use new associations for control flow logic.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,16 +10,15 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, parent_attributes: [:name, :team_name]])
-    # devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
   end
 
   # TODO Review with Jake where helper methods are best implemented
 
   def account_from_current_user
-    if current_user.account_type == "Parent"
-      Parent.find_by(id: current_user.account_id)
-    elsif current_user.account_type == "Child"
-      Child.find_by(id: current_user.account_id)
+    if !current_user.parent_id.nil?
+      Parent.find_by(id: current_user.parent_id)
+    elsif !current_user.child_id.nil?
+      Child.find_by(id: current_user.child_id)
     else
       nil
     end
