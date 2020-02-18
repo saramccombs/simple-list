8b4e79e317644746e7af3c06a048efd2ed3615b2
saramccombs <theSaraMcCombs@gmail.com>
2020-02-18 10:59:16 -0500

    Removed no longer needed code.

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -2,22 +2,4 @@ class ParentsController < ApplicationController
   def new
     @parent = Parent.new
   end
-
-  def create
-    @parent = Parent.new(parent_params)
-    @parent.user_id = current_user.id 
-    @parent.save
-
-    current_user.account_id = @parent.id
-    current_user.account_type = @parent.class
-    current_user.save
-
-    redirect_to root_url
-  end
-
-  private
-
-  def parent_params
-    params.require(:parent).permit(:name, :team_name)
-  end
 end
