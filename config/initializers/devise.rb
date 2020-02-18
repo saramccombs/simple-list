d0b10d45b1b76c1f6457f9250db1c462fb84d019
saramccombs <theSaraMcCombs@gmail.com>
2020-02-18 11:21:19 -0500

    Declared a scope for github omniauth.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,5 +297,5 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  config.omniauth :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user"
 end
