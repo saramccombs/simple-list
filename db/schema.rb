26110e7ee9e533f97445e594745948f65ed8f543
saramccombs <theSaraMcCombs@gmail.com>
2020-02-19 14:33:47 -0500

    Stubbed out ideaboard views, created ideaboard model via migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_170225) do
+ActiveRecord::Schema.define(version: 2020_02_19_192858) do
+
+  create_table "ideaboards", force: :cascade do |t|
+    t.string "ideaboard_name"
+    t.string "ideaboard_desc"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_ideaboards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_02_15_170225) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ideaboards", "users"
 end
