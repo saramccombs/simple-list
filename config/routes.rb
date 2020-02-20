02f1fd2857c24cf93624a13b1ede93475ee80d5d
saramccombs <theSaraMcCombs@gmail.com>
2020-02-20 15:53:32 -0500

    Added blank line after nested resources.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       end
     end
   end
+  
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/users/:id', to: 'application#show', as: 'user'
