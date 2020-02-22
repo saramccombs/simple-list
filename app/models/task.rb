5f02822913c2488312aa145de11b90e9f6c09647
saramccombs <theSaraMcCombs@gmail.com>
2020-02-21 23:45:36 -0500

    Updated association for has many through, should have said has many categories but said has many tasks.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   belongs_to :list
   belongs_to :user
   has_many :categories_tasks
-  has_many :tasks, through: :categories_tasks
+  has_many :categories, through: :categories_tasks
 
   validates :task_priority, presence: { message: "A task priority is required."}
   validates :task_desc, presence: { message: "A task description is required."}
